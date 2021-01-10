263a629b1735d3c870f9cd16610e552931d6a5f1
zakharych <alexzlkl@yandex.ru>
2021-01-10 18:19:02 +0300

    fix: change section place? icons in format to img

--- js/slider.js
+++ js/slider.js
@@ -1,40 +1,50 @@
 
 
 $(document).ready(function(){
-    $('.speakers__list').slick({
+  const history__list = document.querySelector('.history__list')
+  console.log(history__list.children.length);
+
+  $('.history__list').slick({
     infinite: false,
-    speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    rows: 2,
-    dots: false,
-    responsive: [
-      {
-        breakpoint: 1060,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 830,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    slidesToShow: 11,
+    slidesToScroll: 1,
+    draggable: false,
   });
+
+  //   $('.speakers__list').slick({
+  //   infinite: false,
+  //   speed: 300,
+  //   slidesToShow: 5,
+  //   slidesToScroll: 5,
+  //   rows: 2,
+  //   dots: false,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1060,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: false
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 830,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //     // You can unslick at a given breakpoint now by adding:
+  //     // settings: "unslick"
+  //     // instead of a settings object
+  //   ]
+  // });
 });
